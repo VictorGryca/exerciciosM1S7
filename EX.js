5779a8b91b10d4a1438774cc7cdd5487e1312e9c
Victor Grycajuk <victorgryca@gmail.com>
2025-03-28 11:17:04 -0300

    add moto

--- EX.js
+++ EX.js
@@ -22,7 +22,7 @@ class Carro extends Veiculo {
     consumo(){
         switch (this.nPassageiros){
             case 0:
-                console.log("Ninguém esta dirigindo")
+                console.log("Ninguém esta dirigindo o carro")
                 break
             case 1:
                 this.consumoCalculado = this.distanciaPercorrida/this.gasolinaGasta
@@ -32,12 +32,55 @@ class Carro extends Veiculo {
                 this.consumoCalculado = (this.distanciaPercorrida/this.gasolinaGasta)*0.9
                 console.log("Consumo para 2 pessoas: " + this.consumoCalculado + " km/l")
                 break
+            case 3:
+                this.consumoCalculado = (this.distanciaPercorrida/this.gasolinaGasta)*0.8
+                console.log("Consumo para 3 pessoas: " + this.consumoCalculado + " km/l")
+                break
+            case 4:
+                this.consumoCalculado = (this.distanciaPercorrida/this.gasolinaGasta)*0.7
+                console.log("Consumo para 4 pessoas: " + this.consumoCalculado + " km/l")
+                break
+            case 5:
+                this.consumoCalculado = (this.distanciaPercorrida/this.gasolinaGasta)*0.6
+                console.log("Consumo para 5 pessoas: " + this.consumoCalculado + " km/l")
+                break
+            default:
+                console.log("Quantidade de parrageiros inválida.")
+            
         }
     }
 }
 
+class Moto extends Veiculo {
+    constructor(distanciaPercorrida, tempo, nPassageiros, gasolinaGasta){
+        super(distanciaPercorrida, tempo, nPassageiros)
+        this.gasolinaGasta = gasolinaGasta
+        this.consumoCalculado = 0
+    }
 
+    consumo(){
+        switch (this.nPassageiros){
+            case 0:
+                console.log("Ninguém esta dirigindo a moto")
+                break
+            case 1:
+                this.consumoCalculado = this.distanciaPercorrida/this.gasolinaGasta
+                console.log("Consumo para 1 pessoa: " + this.consumoCalculado + " km/l")
+                break
+            case 2:
+                this.consumoCalculado = (this.distanciaPercorrida/this.gasolinaGasta)*0.6
+                console.log("Consumo para 2 pessoas: " + this.consumoCalculado + " km/l")
+                break
+            default:
+                console.log("Quantidade de parrageiros inválida.")
+            
+        }
+    }
+}
 
-const onix = new Carro(2, 1, 1, 1)
+const onix = new Carro(2, 1, 5, 1)
 onix.consumo();
+const honda = new Moto(2,1,2,1)
+honda.consumo()
+
 
